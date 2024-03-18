Fixed SD LSTM, SD LSTM normalization and added token training

SD_LSTM/sd_lstm_model.py
@@ -1,19 +1,16 @@
 import torch
 import torch.nn as nn
 
-# import sys
-# sys.path.append('../utils')
-# Makes more sense for the training function to instantiate this
-# from sd_loss_computation import PerpCalculator
 
 class ConditionalSDLSTM(nn.Module):
-    def __init__(self, input_size, property_size, property_names, hidden_size, output_size, n_layers, rnn_dropout, max_rules, rules_dict_size) -> None:
+    def __init__(self, input_size, property_size, property_names, pnorm_means, pnorm_stds, 
+                hidden_size, output_size, n_layers, rnn_dropout, max_rules, rules_dict_size) -> None:
         """
         LSTM model for training on masked grammar encodings as in SD-VAE. Works like the regular CLSTM model
         but holds and records SDLSTM related parameters
 
         Args:
-            max_rules: Maximum no rules to appy
+            max_rules: Maximum no rules to apply
             rules_dict_size: Length of one hots for encodings, input size
             property_size: number of molecule properties
             hidden_size: number of hidden units
@@ -32,26 +29,47 @@ class ConditionalSDLSTM(nn.Module):
         self.n_layers = n_layers
         self.rnn_dropout = rnn_dropout
         self.property_names = property_names
+        self.pnorm_means = pnorm_means
+        self.pnorm_stds = pnorm_stds
+
 
         # Model originally expected input to be tensors of SMILES tokens (ints) and embedded them before passing to hiden
         # Since we are working with one hot vectors, we can replace this with a linear layer of the approp dimensionality 
 
         # LOUIS: TODO: OLD
-        # self.encoder = nn.Embedding(input_size, hidden_size)
-        self.linear = nn.Linear(input_size, hidden_size)
+        self.encoder = nn.Embedding(input_size, hidden_size)
+        # self.linear = nn.Linear(input_size, hidden_size)
 
         self.decoder = nn.Linear(hidden_size, output_size)
         self.rnn = nn.LSTM(input_size=(hidden_size + property_size), hidden_size=(hidden_size), batch_first=True, num_layers=n_layers, dropout=rnn_dropout)
         
+        # ???? TODO: What does this do?
         self.init_weights()
 
+    def normalize_prop_scores(self, properties):
+        '''
+        Auxiliary function to be used by trainer/sampler class
+        properties is torch.tensor of size num_scores x num_properties
+
+        returns normalized properties
+        '''
+        assert properties.shape[-1] == self.property_size
+
+
+        for i in range(self.property_size):
+            # Apply ith index of self.pnorm_means and self.pnorm_stds to normalize every ith element of the 
+            # num_properties dimension of properties
+            properties[:, i] -= self.pnorm_means[i]
+            properties[:, i] /= self.pnorm_stds[i]
+        
+        return properties
 
 
     def init_weights(self):
         # encoder / decoder
         # LOUIS: TODO: OLD
-        nn.init.xavier_uniform_(self.linear.weight)
-        # nn.init.xavier_uniform_(self.encoder.weight)
+        # nn.init.xavier_uniform_(self.linear.weight)
+        nn.init.xavier_uniform_(self.encoder.weight)
 
         nn.init.xavier_uniform_(self.decoder.weight)
         nn.init.constant_(self.decoder.bias, 0)
@@ -68,10 +86,15 @@ class ConditionalSDLSTM(nn.Module):
                 nn.init.constant_(r_gate, 1)
 
     def forward(self, x, properties, hidden):
+        # Get indecies
+        indices = torch.argmax(x, dim=-1)
         # LOUIS: TODO: REMOVE
-        # embeds = self.encoder(x)
-        embeds = self.linear(x)
-        output, hidden = self.rnn(torch.cat((embeds, properties.unsqueeze(1).expand(-1, x.shape[1], -1)), -1), hidden)
+        embeds = self.encoder(indices)
+        # embeds = self.linear(x)
+
+        inputs = torch.cat((embeds, properties.unsqueeze(1).expand(-1, x.shape[1], -1)), -1)
+
+        output, hidden = self.rnn(inputs, hidden)
         output = self.decoder(output)
         return output, hidden
 
@@ -91,5 +114,7 @@ class ConditionalSDLSTM(nn.Module):
                     n_layers=self.n_layers,
                     rnn_dropout=self.rnn_dropout,
                     max_rules=self.max_rules,
-                    rules_dict_size=self.rules_dict_size)
-
+                    rules_dict_size=self.rules_dict_size,
+                    pnorm_means=list(self.pnorm_means),
+                    pnorm_stds=list(self.pnorm_stds)
+                    )

SD_LSTM/sd_lstm_sampler.py
@@ -9,207 +9,242 @@ import torch.nn.functional as F
 from sd_lstm_model import ConditionalSDLSTM
 from sd_lstm_utils import load_model
 
+from torch import nn
+
 import sys
 import os
 sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)) , '../utils'))
 from sd_smiles_decoder import raw_logit_to_smiles
 
+from torch.distributions import Categorical
+
+from tree_walker import ConditionalDecoder
+from attribute_tree_generator import create_sequential_tree_decoder, FETCH_TOKEN, RETURN_TOKEN
+
+from mol_tree import Node, get_smiles_from_tree
+
+
+FAILED_TO_TERM_ERR_STR = 'ERROR-STILL-DECODING'
+FAILED_TO_DECODE_ERR_STR = 'ERROR-FAILED-TO-CONSTRUCT-SMILE'
 
 class SDLSTMSampler:
-    def __init__(self, batch_size, device, rules_dict_size):
+    def __init__(self, batch_size, device, rules_dict_size, distribution_cls = Categorical):
         self.batch_size = batch_size
         self.device = device
         self.rules_dict_size = rules_dict_size
-
-    '''
-    def sample_batch_constrained(self, model, max_rules, property_values):
-        # Autoregressively sample a batch of smiles enforcing rule validity at each step 
-        # model: SDLSTM model
-        # max_rules: maximum number of grammar rules (sequence length before SMILES decoding)
-        # property_values: torch.Tensor( [list of property values])
-        
-        model.eval()
-        torch.no_grad()
-
-        model = model.to(self.device)
-        max_rules -= 1
-
-        initial_input = torch.zeros(self.batch_size, 1, self.rules_dict_size).to(self.device)
-        initial_input[:, 0, 0] = 1
-
-        properties = property_values.repeat(self.batch_size, 1).to(self.device)
-        hidden = model.init_hidden(self.batch_size, self.device)
-
-        rules_list = []
-        logits_list = []
-        current_input = initial_input
-
-        for _ in range(max_rules):
-            # Get logits for next step
-            
-            # Logits shape is torch.Size([64, _, 80]), next char logits are final index
-            logits, hidden = model(current_input, properties, hidden)
-
-            # Final index is logits for next step
-            next_step_logits = logits[:, -1, :]
-
-            walker = ConditionalDecoder(np.squeeze(pred_logits), use_random)
-
-            # Check validity
+        self.distribution_cls = distribution_cls
 
 
-    for i in range(raw_logits.shape[1]):
-        pred_logits = raw_logits[:, i, :]
-        walker = ConditionalDecoder(np.squeeze(pred_logits), use_random)
-        new_t = Node('smiles')
-        try:
-            tree_decoder = create_tree_decoder()
-            tree_decoder.decode(new_t, walker)
-            sampled = get_smiles_from_tree(new_t)
-            
-        except Exception as ex:
-            if not type(ex).__name__ == 'DecodingLimitExceeded':
-                print('Warning, decoder failed with', ex)
+    def pad_one_hots(self, one_hots, max_rules):
+        padding_length = max_rules - one_hots.shape[0]
+        if padding_length > 0:
+                # Create a one-hot encoded vector for the padding token
+            padding_vector = np.zeros((1, one_hots.shape[1]), dtype=np.float32)  # Initialize with zeros
+            padding_vector[0, model.rules_dict_size - 1] = 1  # Set the appropriate index for the padding token to 1
 
-            # failed. output None
-            sampled = None
- 
-        if sampled is None:
-            continue
-        
-        # we generated a good one
-        generations.append(sampled)
+            # Create a padding array with the desired padding length
+            padding_array = np.tile(padding_vector, (padding_length, 1))  # Repeat the padding vector
+
+            # Append the padding array to the original sequence of one-hots
+            padded_one_hots = np.concatenate((one_hots, padding_array), axis=0)
+            return padded_one_hots
+        else:
+            return one_hots
 
 
-            # logits_list.append(logits[:, -1:, :])
-
-            print()
-            print()
-            print(logits.shape)
-            print(logits)
-            print()
-            print()
-            print(logits_list)
-
-            return
-            # Check validity and 
-            
-            # Construct next input
-            current_input = logits[:, -1:, :]
-
-        # Concatenate the logits from each timestep to form the sequence
-        raw_logits = torch.cat(logits_list, dim=1)
-    '''
-
-
-    def sample_batch_unconstrained_logits(self, model, max_rules, property_values):
+    def sample_batch_masking_smiles(self, model, max_rules, property_val):
         '''
-        Autoregressively sample a batch of logits from the model without enforcing rule validity at each step.
-
-        device: string device ('cpu' | 'gpu')
-        Properties: torch.Tensor([List of pvals for all generations) or torch.Tensor(single prop value to be repeated)
-        batch_size: size of batch to sample
+        Sample a batch of smiles with property values described in property_val. If max_decoding limit reached without
+        termination return ERROR instead of that smiles
+        
+        - model : model
+        - max_rules : int max decoding steps
+        - property_val : torch.Tensor of shape num_to_sample x num_props containing properties 
+        return smiles (arr str containing sampled smiles)
         '''
+        assert len(property_val.shape) == 2 and property_val.shape[1] == model.property_size
 
+
+        # Set model to evaluation mode
         model.eval()
         with torch.no_grad():
-            # TODO: Repeat for number of samples
+            # Infer batch size
+            b_size = property_val.shape[0]
+
+            # Initialize model inputs and send to device
             model = model.to(self.device)
-
-            # We will set the initial token
-            max_rules -= 1
-
-            # Prepare initial inputs: using zeros as initial inputs
-            initial_input = torch.zeros(self.batch_size, max_rules, self.rules_dict_size).to(self.device)
-
-            # Set first index to initial token
-            initial_input[:, 0, 0] = 1
+            property_values = torch.Tensor(property_val).to(self.device)
+            hidden = model.init_hidden(b_size , self.device)
+            # Current input for model
             
-            # Create properties array
-            if  property_values.shape[0] == 1:
-                properties = property_values.repeat(self.batch_size, 1).to(self.device)
+            # Initialize a tensor of zeros with the shape [b_size, model.rules_dict_size]
+            current_input = torch.zeros([b_size, 1 , model.rules_dict_size], dtype=torch.float, device=self.device)
+
+            # Set the first element of each row in current_input to 1
+            current_input[:, 0] = 1
+            
+            # Array fo walkers, one for each element of batch_size
+            still_executing = [True] * b_size
+            walkers = []
+            root_nodes = []
+            dec_generators = []
+            for _ in range(b_size):
+                # INIT WALKERS
+                onehot_start = torch.zeros(1, model.rules_dict_size)
+                onehot_start[0][0] = 1
+                new_walker = ConditionalDecoder(onehot_start, use_random=False)
+                walkers.append(new_walker)
+                new_node = Node('smiles')
+                root_nodes.append(new_node)
+                tree_decoder = create_sequential_tree_decoder()
+                dec_gen = tree_decoder.decode(new_node, new_walker)
+                # Burn initial logit / input
+                fmsk, frsp = next(dec_gen)
+                dec_generators.append(dec_gen)
+                # TODO: Remove this once I'm confident it's working properly
+                assert frsp == FETCH_TOKEN and fmsk == [0]
+
+            # TODO: Remove Debugging code
+            tokens = [[] for _ in range(b_size)]
+
+            for rindex in range(max_rules):
+                '''
+                PROCESS:
+                1. Next to get next mask
+                2. Compute logits, mask them and add them to the walker
+                3. Continue execution
+                '''
+                logits, hidden = model(current_input, property_values, hidden)
+                # Fetch Next Mask
+                # Fetch next masks
+                for ind in range(b_size):
+                    if not still_executing[ind]:
+                        # Already finished executing
+                        continue
+                    # Get mask and check still running
+                    
+                    # TODO: Remove debugging code
+                    # print(tokens[ind])
+
+                    mind, resp = next(dec_generators[ind])
+                    if resp == RETURN_TOKEN:
+                        still_executing[ind] = False
+                        # We've just finished, no need to sample any more
+                        continue
+
+                    mask = torch.zeros(model.rules_dict_size, dtype=torch.bool)
+                    mask[mind] = True
+                    cur_step_logits = logits[ind][0]
+                    # Logits at step nexst index
+                    cur_step_logits[~mask] = float('-inf')
+
+                    # Compute probabilities
+                    cur_exped = np.exp(cur_step_logits - cur_step_logits.max())
+                    cur_prob = cur_exped / (cur_exped.sum() + 1e-8)
+
+                    distribution = self.distribution_cls(probs=cur_prob)
+                    new_tok = distribution.sample().item()
+                    new_log = torch.zeros(1, model.rules_dict_size)
+                    new_log[0][new_tok] = 1
+
+                    walkers[ind].raw_logits = np.append(walkers[ind].raw_logits, new_log, axis=0)
+
+                    current_input[ind] = new_log.clone().detach()
+                    
+                    # TODO: REMOVE DEBUGGING CODE
+                    # tokens[ind].append(new_tok)
+
+
+        smiles = []
+        # Compute smiles
+        for ind in range(b_size):
+            # If hasn't finished decoding, set to error
+            if still_executing[ind]:
+                smiles.append(FAILED_TO_TERM_ERR_STR)
             else:
-                assert property_values.shape[0] == self.batch_size 
-            
+                try:
+                    smiles.append(get_smiles_from_tree(root_nodes[ind]))
+                except:
+                    smiles.append(FAILED_TO_DECODE_ERR_STR)
+        return smiles
 
-            hidden = model.init_hidden(self.batch_size, self.device)
-
-            # Forward pass to generate raw logits
-            # Since your model expects inputs at each step, you can loop through the sequence length,
-            # feeding the output back as input at each step
-            logits_list = []
-
-            current_input = initial_input
-
-            for _ in range(max_rules):
-                logits, hidden = model(current_input, properties, hidden)  # Forward pass
-                logits_list.append(logits[:, -1:, :])  # Append the last timestep's logits
-
-                # Prepare the next input
-                # Here, you might want to apply some sampling strategy to convert logits to discrete tokens if necessary
-                # For simplicity, we're using the logits directly as the next input
-                current_input = logits[:, -1:, :]
-
-            # Concatenate the logits from each timestep to form the sequence
-            raw_logits = torch.cat(logits_list, dim=1)
-
-            return raw_logits.detach()
-
-
-    def sample_batch_unconstrained_smiles(self, model, max_rules, property_values, quiet=False):
+    def sample(self, model, properties, num_to_sample, max_seq_len, quiet=True):
         '''
-        Generate logits autoregressively, then enforce constraints during parse time
-
-        Return:
-        One batch of smiles strings
+        Interface for evaluation code
         '''
-        logits = self.sample_batch_unconstrained_logits(model = model, max_rules = max_rules, property_values = property_values)
-        # input expected to be array
-        return raw_logit_to_smiles(np.array(logits.permute(1, 0, 2)), use_random=True, quiet=quiet)
+        assert properties.shape[0] == num_to_sample
 
+        properties = model.normalize_prop_scores(properties)
 
-    def sample(self, model, properties, num_to_sample, max_seq_len, mode='UNC', quiet=True, force = False):
-        '''
-        mode : 'UNC' for unconstrained logit generation OR 'CONST' for constrained sequential generation
-        properties : torch.Tensor[[prop_value to be repeated]] or torch.Tensor([[properties] * num_to_sample])
-        Wrapper function for compatibility with vanilla ConditionalLSTM sampler
-        '''
-        assert mode in ['UNC', 'CONST']
+        smiles = []  # finished smiles
+        num_batches = (num_to_sample + self.batch_size - 1) // self.batch_size
 
-        if mode == 'CONST':
-            raise Exception('Constrained sampling not implemented. you should implement it TODO:')
-
-        if not force:
-            n_batches = math.ceil(num_to_sample / self.batch_size)
-            smiles = []
-            # for batch in range(n_batches):
-            for i in range(n_batches):
-                smiles += self.sample_batch_unconstrained_smiles(model=model, max_rules = max_seq_len, property_values=properties[0], quiet=quiet)
+        if quiet:
+            itr = range(num_batches)
         else:
-            smiles = []
-            while len(smiles) < num_to_sample:
-                smiles += self.sample_batch_unconstrained_smiles(model=model, max_rules = max_seq_len, property_values=properties[0], quiet=quiet)
+            itr = tqdm(range(num_batches))
 
-        return smiles[:num_to_sample]
+        for batch_idx in itr:
+            start_idx = batch_idx * self.batch_size
+            end_idx = min((batch_idx + 1) * self.batch_size, num_to_sample)
 
+            properties_batch = properties[start_idx:end_idx]
+            batch_smiles = self.sample_batch_masking_smiles(model, max_seq_len, properties_batch)
+            smiles.extend(batch_smiles)
+
+        return smiles
+
+from rdkit import Chem
+from tqdm import tqdm
+
+import sys
+sys.path.append('../utils/')
+from property_calculator import PropertyCalculator
+pc = PropertyCalculator(['LogP'])
+
+def props_from_smiles(smiles_list, verbose=True):
+    '''
+    Computes property scores of all valid smiles and returns as list
+    '''
+    if verbose:
+        smiles_list = tqdm(smiles_list)
+    props = []
+    for smi in smiles_list:
+        mol = Chem.MolFromSmiles(smi)
+        if mol is not None:
+            props.append(pc(mol)[0])
+    return props
 
 
 if __name__ == '__main__':
-    model_weights = '../models/SD_LSTM_QM9_BCE/batch_size_64_1/LSTM_40_1.489.pt'
-    model_definit = '../models/SD_LSTM_QM9_BCE/batch_size_64_1/LSTM_40_1.489.json'
+    # model_weights = '../models/SD_LSTM_FR/SD_LSTM_silent-queen-29_Epoch_5_Vl_0.123.pt'
+    # model_definit = '../models/SD_LSTM_FR/SD_LSTM_silent-queen-29_Epoch_5_Vl_0.123.json'
+    model_weights = '../models/SD_LSTM_QM9_MASKED_CROSS_ENTROPY_TOKENS/SD_LSTM_dark-hall-89_Epoch_20_Vl_0.201.pt'
+    model_definit = '../models/SD_LSTM_QM9_MASKED_CROSS_ENTROPY_TOKENS/SD_LSTM_dark-hall-89_Epoch_20_Vl_0.201.json'
 
-    sampler = SDLSTMSampler(batch_size = 64, device = 'cpu', rules_dict_size = 80)
-
+    sampler = SDLSTMSampler(batch_size = 128, device = 'cpu', rules_dict_size = 80)
     model = load_model(model_definit, model_weights, device='cpu')
 
-    print(sampler.sample( model=model, num_to_sample=100,  properties= torch.Tensor([[2.5] for _ in range(100)]),max_seq_len=100 ))
+    max_rules = 100
+    property_val = -3
 
-    #print(sampler.sample_batch_unconstrained_logits(model=model, max_rules=100, property_values=torch.Tensor([1.0])))
-    # print(sampler.sample(model=model, num_to_sample = 100, properties=torch.Tensor([[1], [0]]), max_seq_len = 100))
+    n_props = 100
+    import random
+    # random.uniform(-3, 3)
+    properties = torch.tensor([[property_val] for _ in range(n_props)])
+    properties = properties.to(torch.float32)
 
-    #TODO: Test if max_seq_len is negotiable
-    # sample = sampler.sample(model=model, properties=torch.Tensor([[1.0]]), num_to_sample=100, max_seq_len=100)
-    # print(len(sample))
-    # print(sample)
+    smiles = sampler.sample(model, properties, n_props, 100 + 50, quiet=False)
+    # Print non terminated
+    non_termed = smiles.count(FAILED_TO_TERM_ERR_STR)
+    print(f'Didnt terminate: {non_termed}')
+    # Print other error
+    failed_decode = smiles.count(FAILED_TO_DECODE_ERR_STR)
+    print(f'Failed to decode: {failed_decode}')
 
-    # This strategy creates all the same molecule ... use_random set to true causes errors ...
+    unique_pct = len(set(smiles)) / len(smiles)
+    print(f'Unique PCT {unique_pct}')
+
+    calprops = props_from_smiles(smiles)
+    avg_prop = np.mean(calprops)
+    print(f'{avg_prop} target: {property_val}')

SD_LSTM/train_sd_lstm.py
@@ -1,6 +1,3 @@
-'''
-
-'''
 
 from sd_lstm_model import ConditionalSDLSTM
 import torch
@@ -16,7 +13,7 @@ from tqdm import tqdm
 
 sys.path.append('../utils/')
 from sd_lstm_utils import load_encodings_masks_and_properties, get_tensor_dataset, get_tensor_dataset_alt, save_model
-from sd_loss_computation import my_perp_loss
+from sd_loss_computation import my_perp_loss, my_binary_loss, PerpCalculator, masked_ce
 
 
 from haikunator import Haikunator
@@ -24,18 +21,30 @@ import json
 
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 
+from torch.nn.utils import clip_grad_norm_
+
 
 class SD_LSTM_Trainer:
-    def __init__(self, train_data_loader, val_data_loader, model=None, device = 'cpu', batch_size = None, 
+    def __init__(self, train_x_enc, train_x_masks, train_y, valid_x_enc, valid_x_masks, valid_y, property_names,
+                 model=None, device = 'cpu', batch_size = None, 
                  hidden_size = 512, n_layers = 3, rnn_dropout = 0.2, learning_rate = 1e-3,
                  rules_dict_size = 80, max_seq_len = 100, 
                  model_save_dir = '../models/SD_LSTM_FR/', 
                  valid_every_n_epochs = 1, save_every_n_val_cycles = 3, max_epochs = 100):
+        
+        all_y = np.concatenate((train_y, valid_y), axis=0)
+
+        mean = np.mean(all_y, axis = 0)
+        std = np.std(all_y, axis = 0)
+
+        n_props = train_y.shape[-1] # Should be 1
 
         if model == None:
             self.model = ConditionalSDLSTM(input_size=rules_dict_size,
                 property_size=n_props,
                 property_names=property_names,
+                pnorm_means=mean,
+                pnorm_stds=std,
                 hidden_size=hidden_size,
                 output_size=rules_dict_size,
                 n_layers=n_layers,
@@ -45,9 +54,17 @@ class SD_LSTM_Trainer:
                 )
         else:
             self.model = model.to(device)
+
+        train_y = self.model.normalize_prop_scores(train_y)
+        valid_y = self.model.normalize_prop_scores(valid_y)
         
-        self.train_data_loader = train_data_loader
-        self.val_data_loader = val_data_loader
+        train_set = get_tensor_dataset(train_x_enc, train_x_masks, train_y)
+        valid_set = get_tensor_dataset(valid_x_enc, valid_x_masks, valid_y)
+
+        self.train_data_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
+        self.val_data_loader = DataLoader(valid_set, batch_size=batch_size, shuffle=True)
+
+        # TODO: MAke sure this works
         self.device = device
         self.batch_size = batch_size
         self.hidden_size = hidden_size
@@ -59,7 +76,9 @@ class SD_LSTM_Trainer:
         self.save_every_n_val_cycles = save_every_n_val_cycles
 
         self.optimizer = torch.optim.Adam(self.model.parameters(), lr=learning_rate)
-        self.loss_fnct = my_perp_loss
+
+        self.loss_fnct = masked_ce
+
         self.max_epochs = max_epochs
 
         self.train_losses = []
@@ -73,7 +92,6 @@ class SD_LSTM_Trainer:
             self.batch_size = batch_size
 
         os.makedirs(self.model_save_dir, exist_ok=True)
-
 
         # self.lr_scheduler = ReduceLROnPlateau(self.optimizer, 'min', factor=0.5, patience=5, min_lr=0.001)
 
@@ -108,7 +126,7 @@ class SD_LSTM_Trainer:
             'rules_dict_size': self.model.input_size,  # Assuming input_size is accessible in your model
             'max_seq_len': self.model.max_rules,  # Assuming max_rules corresponds to max sequence length in your model
             'valid_every_n_epochs': self.valid_every_n_epochs,
-            'save_every_n_val_cycles': self.save_every_n_val_cycles,
+            'save_every_n_val_cycles': self.save_every_n_val_cycles
         }
         
         # Write the parameters dictionary to a JSON file
@@ -127,9 +145,7 @@ class SD_LSTM_Trainer:
 
         self.save_params(run_name=run_id)
 
-
         print(f'Training run ID: {run_id}')
-
 
         best_val_loss = np.inf
         # Save training parameters
@@ -190,12 +206,14 @@ class SD_LSTM_Trainer:
             # tgt is shifted by 1 forwards inputs: batch x (max_seq_len - 1) x decision_dim 
             
             # Compute loss : TODO: Reorder dimensions? 
-            batch_loss = self.loss_fnct(tgt, mask, output_logits) / data_loader.batch_size
+
+            batch_loss = self.loss_fnct(tgt, mask, output_logits)[0] # This is not neccesary/ data_loader.batch_size
 
             # We therefore expect batch_loss to be of dim batch_size x 1
             if phase == 'train':
                 self.optimizer.zero_grad()
                 batch_loss.backward()
+
                 self.optimizer.step()
 
             # Detach batch loss to save memory
@@ -226,7 +244,6 @@ class SD_LSTM_Trainer:
         return avg_loss
 
 
-
 if __name__ == "__main__":
         data_path = '../data/QM9/'
         rules_dict_size = 80
@@ -237,36 +254,24 @@ if __name__ == "__main__":
         rules_dict_size = rules_dict_size
         n_layers = 3
         rnn_dropout = 0.2
-        
-        batch_size = 300
-
+        batch_size = 64
         learning_rate = 1e-3
 
         # Load Property Data and Masks and create splits
         property_names, train_x_enc, train_x_masks, train_y, valid_x_enc, valid_x_masks, valid_y = load_encodings_masks_and_properties(data_path)
-        n_props = len(property_names)
 
-        all_y = np.concatenate((train_y, valid_y), axis=0)
-        mean = np.mean(all_y, axis = 0)
-        std = np.std(all_y, axis = 0)
+        trainer = SD_LSTM_Trainer(train_x_enc=train_x_enc,
+                                train_x_masks=train_x_masks,
+                                train_y=train_y,
+                                valid_x_enc=valid_x_enc,
+                                valid_x_masks=valid_x_masks,
+                                valid_y=valid_y,
+                                property_names=property_names,
+                                model_save_dir='../models/SD_LSTM_QM9_MASKED_CROSS_ENTROPY_TOKENS',
+                                n_layers=n_layers,
+                                batch_size=batch_size)
 
-        train_y = (train_y - mean) / std
-        valid_y = (valid_y - mean) / std
-        
-        train_set = get_tensor_dataset_alt(train_x_enc, train_x_masks, train_y)
-        valid_set = get_tensor_dataset_alt(valid_x_enc, valid_x_masks, valid_y)
+        # Save some memory in case we're running on CPU, delete external refs to dead objects
+        del train_x_enc, train_x_masks, valid_x_enc, valid_x_masks, train_y, valid_y
 
-        max_rules = train_x_enc.shape[1] # def 100
-        rules_dict_size = train_x_enc.shape[2] # def 80
-
-        #TODO: figure out something with batch size so we aren't passing the param twice
-
-        train_data_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
-        val_data_loader = DataLoader(valid_set, batch_size=batch_size, shuffle=True)
-
-        # Save some memory in case we're running on CPU
-        del train_set, valid_set, train_x_enc, train_x_masks, valid_x_enc, valid_x_masks, train_y, valid_y
-
-        trainer = SD_LSTM_Trainer(train_data_loader = train_data_loader, val_data_loader = val_data_loader, n_layers=1, batch_size=32)
         trainer.fit()
-        

utils/sd_loss_computation.py
@@ -11,9 +11,36 @@ sys.path.append('../')
 
 from config import CONFIG
 
+CE_CAL = torch.nn.CrossEntropyLoss(ignore_index=79)
+
+def masked_ce(true_one_hots, mask, logits, use_mask=True):
+    # Convert targets from onehots to numbers
+    '''
+    true_one_hots is b_size x seq_len x decision_dim
+    mask is b_size x seq_len x decision_dim
+    logits is b_size x seq_len x decision_dim
+    '''
+    true_tokens = torch.argmax(true_one_hots, dim=-1).long()
+
+    # Mask logits
+    if use_mask:
+        logits = logits + (mask - 1) * 1e9
+
+    # Collapse dimensions
+    logits = logits.view(-1, logits.size(-1))  # Reshape to [64*99, 80]
+    true_tokens = true_tokens.view(-1)
+    
+    return [CE_CAL(logits, true_tokens)]
+
+
 class PerpCalculator(nn.Module):
-    def __init__(self):
+    def __init__(self, loss_type = None):
         super(PerpCalculator, self).__init__()
+
+        if loss_type is None:
+            self.loss_type = CONFIG.loss_type
+        else:
+            self.loss_type = loss_type
     '''
     input:
         target (int 65), loss (float32), output (float32)    [50, 99, 80]  seq_len x batch_size x decision_dim
@@ -24,7 +51,7 @@ class PerpCalculator(nn.Module):
     '''
     def forward(self, true_binary, rule_masks, raw_logits):
         true_binary = true_binary.float()
-        if CONFIG.loss_type == 'binary':
+        if self.loss_type == 'binary':
             exp_pred = torch.exp(raw_logits) * rule_masks
 
             norm = F.torch.sum(exp_pred, 2, keepdim=True)
@@ -32,10 +59,10 @@ class PerpCalculator(nn.Module):
 
             return [F.binary_cross_entropy(prob, true_binary) * CONFIG.max_decode_steps]
 
-        if CONFIG.loss_type == 'perplexity':
+        if self.loss_type == 'perplexity':
             return my_perp_loss(true_binary, rule_masks, raw_logits)
 
-        if CONFIG.loss_type == 'vanilla':
+        if self.loss_type == 'vanilla':
             exp_pred = torch.exp(raw_logits) * rule_masks + 1e-30
             norm = torch.sum(exp_pred, 2, keepdim=True)
             prob = torch.div(exp_pred, norm)
@@ -47,7 +74,8 @@ class PerpCalculator(nn.Module):
             loss = -torch.sum(logll) / true_binary.size()[1]
             
             return loss
-        print('unknown loss type %s' % CONFIG.loss_type)
+
+        print('unknown loss type %s' % self.loss_type)
         raise NotImplementedError 
 
 
@@ -58,21 +86,35 @@ class MyPerpLoss(torch.autograd.Function):
     '''
     @staticmethod
     def forward(ctx, true_binary, rule_masks, input_logits):
+
         ctx.save_for_backward(true_binary, rule_masks, input_logits)
 
+        # Subtract largest logit --> Input dim 2 is decision dim
         b = F.torch.max(input_logits, 2, keepdim=True)[0]
-        raw_logits = input_logits - b
-        exp_pred = torch.exp(raw_logits) * rule_masks + 1e-30
+        raw_logits = input_logits - b # What does subtracting a scalar from a 3d tensor do?
+
+
+        # Why do we do it again ignoring the masks
+        exp_pred = torch.exp(raw_logits) * rule_masks + 1e-30 # Mul w mask, add small constant for stability
+        # exp_pred = torch.exp(raw_logits) + 1e-30
 
         norm = torch.sum(exp_pred, 2, keepdim=True)
         prob = torch.div(exp_pred, norm)
-
+        
+        # This leaves the loss at the true point, since it's the only nonzero true binary value
+        # Likelyhood of true vale
         ll = F.torch.abs(F.torch.sum( true_binary * prob, 2))
         
+        # Subtract the final element of each mask, if it's a final token mask it will yield zero
+        # Final tokens don't contribute to loss
         mask = 1 - rule_masks[:, :, -1]
 
         logll = mask * F.torch.log(ll)
+        # Why we loggin twice?
+        # logll = F.torch.log(ll)
 
+        # The loss is already notmalized by the batch size
+        # The loss is the negative log likelyhood of each batch element
         loss = -torch.sum(logll) / true_binary.size()[1]
         
         if input_logits.is_cuda:
@@ -91,7 +133,9 @@ class MyPerpLoss(torch.autograd.Function):
 
         b = F.torch.max(input_logits, 2, keepdim=True)[0]
         raw_logits = input_logits - b
-        exp_pred = torch.exp(raw_logits) * rule_masks + 1e-30
+
+        # exp_pred = torch.exp(raw_logits) * rule_masks + 1e-30
+        exp_pred = torch.exp(raw_logits) + 1e30
 
         norm = torch.sum(exp_pred, 2, keepdim=True)
         prob = torch.div(exp_pred, norm)
@@ -99,11 +143,14 @@ class MyPerpLoss(torch.autograd.Function):
         grad_matrix1 = grad_matrix2 = None
         
         grad_matrix3 = prob - true_binary
-        bp_mask = rule_masks.clone()
-        bp_mask[:, :, -1] = 0
+        
+        # Removed
+        #bp_mask = rule_masks.clone()
+        #bp_mask[:, :, -1] = 0
 
         rescale = 1.0 / true_binary.size()[1]
-        grad_matrix3 = grad_matrix3 * bp_mask * grad_output.data * rescale        
+        # grad_matrix3 = grad_matrix3 * bp_mask * grad_output.data * rescale
+        grad_matrix3 = grad_matrix3 * grad_output.data * rescale
 
         return grad_matrix1, grad_matrix2, Variable(grad_matrix3)
 
@@ -138,7 +185,7 @@ class MyBinaryLoss(torch.autograd.Function):
         with respect to the output, and we need to compute the gradient of the loss
         with respect to the input.
         """
-        raise NotImplementedError
+        #raise NotImplementedError
         true_binary, rule_masks, input_logits  = ctx.saved_tensors
 
         b = F.torch.max(input_logits, 2, keepdim=True)[0]